8f9733d5ffd91c5adaabcb6262603aa3b92d953e
goose1n <danilsev07t@gmail.com>
2023-03-10 16:51:46 +0600

    modified:   src/App.jsx 	renamed:    src/Pages/BloSin/BloSin.jsx -> src/Pages/BlogSingle/BloSin.jsx 	renamed:    src/Pages/BloSin/BloSin.module.css -> src/Pages/BlogSingle/BloSin.module.css 	renamed:    src/Pages/Change/Change.jsx -> src/Pages/Changelog/Change.jsx 	renamed:    src/Pages/Change/Change.module.css -> src/Pages/Changelog/Change.module.css 	renamed:    src/Pages/Pass/Pass.jsx -> src/Pages/PasswordProtect/Pass.jsx 	renamed:    src/Pages/Pass/Pass.module.css -> src/Pages/PasswordProtect/Pass.module.css 	renamed:    src/Pages/PorSin/PorSin.jsx -> src/Pages/PortfolioSingle/PorSin.jsx 	renamed:    src/Pages/PorSin/PorSin.module.css -> src/Pages/PortfolioSingle/PorSin.module.css 	renamed:    src/Pages/SerSin/SerSin.jsx -> src/Pages/ServiceSingle/SerSin.jsx 	renamed:    src/Pages/SerSin/SerSin.module.css -> src/Pages/ServiceSingle/SerSin.module.css 	new file:   src/Pages/ShopSingle/ShopSingle.jsx 	new file:   src/Pages/ShopSingle/ShopSingle.module.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home';
-import Shop from './Pages/Shop'
 
 function App() {
 
   return (
     <Layout>
-      hello world
       <Routes>
-        <Route path='/home' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/shop' element={<Shop/>} />
-        <Route path='/projects'element={<Project/>} />
-        <Route path='/news'/>
-        <Route path='/error'/>
-        <Route path='/style'/>
-        <Route path='/password'/>
-        <Route path='/licences'/>
-        <Route path='/changelog' element={<Change/>}/>
+        <Route path='/' element='1'/>
+        <Route path='/' element='2'/>
+        <Route path='/' element='3' />
+        <Route path='/' element='4' />
+        <Route path='/' element='5' />
+        <Route path='/' element='6' />
+        <Route path='/' element='7' />
+        <Route path='/' element='8' />
+        
       </Routes>
     </Layout>
     
