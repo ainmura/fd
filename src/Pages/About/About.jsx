422f5b798aa9e64f9e05251ddb9d3392d0ebfffd
Nazima <nazimazamirkulova14@gmail.com>
2023-03-10 14:20:26 +0600

    modified:   src/Pages/About/About.jsx

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <h1>heloo</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugit aut deleniti ea tempora nihil dolore quae vel esse beatae, omnis pariatur autem facilis eaque consequatur saepe impedit ullam praesentium.</p>
+    </div>
   )
 }
 
